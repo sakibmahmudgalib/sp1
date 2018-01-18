3a67f100eb1a00fccaa66f3077c542bc31f1d464
sakib mahmud galib <sakibmahmudgalib@gmail.com>
2018-01-18 15:17:05 +0600

    up

---
+++ header.php
@@ -0,0 +1,245 @@
+<html>
+<head>
+  <title>SP1 Project</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <style>
+    body {
+      background-color: ;
+
+    }
+
+    .one {
+      color: green;
+      padding: 20px;
+    }
+
+    .two {
+      color: ;
+      text-align: right;
+    }
+
+    a {
+      text-decoration: none;
+    }
+
+    .three:hover {
+      text-decoration: none;
+      background-color: #4f4a4a;
+      color: white;
+    }
+
+    .search {
+      color: ;
+      background-color: ;
+
+      padding: 6px 8px;
+
+      margin-right: px;
+      background: #ededed;
+      font-size: 17px;
+      border: none;
+      cursor: pointer;
+
+    }
+
+    #text {
+      background-color: #d8d8d8;
+      border-color: #039fad;
+      font-size: 15px;
+      border: 0px solid #4CAF50;
+
+    }
+
+    .search:hover {
+      color: white;
+      background-color: black;
+
+    }
+
+    div.space {
+      word-spacing: 18px;
+      padding: 4px;
+      font-size: 20px;
+      background-color: #d8d8d8;
+    }
+
+
+    div.sticky1 {
+      position: -webkit-sticky;
+      position: sticky;
+      top: 0;
+      padding: 2px;
+      background-color: #039fad;
+      text-align: center;
+      border: 0px solid #4CAF50;
+
+    }
+
+    .content {
+      padding: 10px;
+      background-color: ;
+    }
+
+
+    .dropdown {
+      position: relative;
+      display: inline-block;
+    }
+
+    .dropdown .dropbtn {
+      font-size: 10px;
+      border: none;
+      outline: none;
+      color: white;
+      padding: 14px 16px;
+      background-color: inherit;
+    }
+
+    .dropdown-content {
+      display: none;
+      position: absolute;
+      background-color: #f9f9f9;
+      min-width: 160px;
+      box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+      z-index: 1;
+    }
+
+    .dropdown-content a {
+      float: none;
+      color: black;
+      padding: 2px 5px;
+      text-decoration: none;
+      display: block;
+      text-align: left;
+    }
+
+    .dropdown-content a:hover {
+      color: #ea7704;
+      background-color: #ddd;
+    }
+
+    .dropdown:hover .dropdown-content {
+      display: block;
+    }
+  </style>
+</head>
+
+<body>
+  <table border="0" width="100%">
+    <tr>
+      <td class="one">
+        <a href="">
+          <font size="5">Brand Logo</font>
+        </a>
+      </td>
+      <td><b><input id="text" type="text" size="80" placeholder="Search..." style="padding:6px"/><a class="search" href="" style="word-spacing:0px" title="Search">  <i style="color:;padding:0px" class="fa fa-search" title="Search"><b>Search</b></i>
+        </a>
+        </b>
+      </td>
+
+      <td class="two">
+        <a href="">
+          <font size="4">Login</font>
+        </a>&nbsp;&nbsp;&nbsp;
+        <a href="">
+          <font size="4">Register</font>
+        </a>&nbsp;&nbsp;&nbsp;
+        <a href="">
+          <font size="4">Contact us</font>
+        </a>
+      </td>
+
+
+    </tr>
+
+  </table>
+  
+  
+  <div class="sticky1">
+    <div class="space">
+      <div class="dropdown">
+
+        <b> <a  class="three" class="dropbtn" href=""style="color:" >  Discover</a></b>
+        <div class="dropdown-content">
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </div>
+
+      <div class="dropdown">
+
+        <b> <a  class="three" class="dropbtn" href=""style="color:">  TopBooks</a></b>
+        <div class="dropdown-content">
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </div>
+      <div class="dropdown">
+        <b> <a  class="three" class="dropbtn" href=""style="color:;word-spacing:0px"> coming Soon</a></b>
+        <div class="dropdown-content">
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </div>
+      <div class="dropdown">
+        <b> <a  class="three" class="dropbtn" href=""style="color:">  Recommended</a></b>
+        <div class="dropdown-content">
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </div>
+      <div class="dropdown">
+        <b><a  class="three" class="dropbtn" href=""style="color:">  Genres</a></b>
+        <div class="dropdown-content">
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </div>
+      <div class="dropdown">
+
+        <b> <a  class="three" class="dropbtn" href=""style="color:"> Reading</a></b>
+        <div class="dropdown-content">
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </div>
+      <div class="dropdown">
+        <b> <a  class="three" class="dropbtn" href=""style="color:"> History</a></b>
+        <div class="dropdown-content">
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </div>
+      <div class="dropdown">
+        <b> <a  class="three" class="dropbtn" href=""style="color:;word-spacing:0px"> Read Later</a></b>
+        <div class="dropdown-content">
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </div>
+      <div class="dropdown">
+        <b><a  class="three" class="dropbtn" href=""style="color:"> Favourates</a></b>
+        <div class="dropdown-content">
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </div>
+      <!--b><input type="text" size="31" placeholder="Search..." style="padding:4px"/><a class="search" href="" style="word-spacing:0px">  <!--button  style="width:45px;padding:2px" title="search"-->
+      <!--i style="color:;padding:0px" class="fa fa-search"></i><!--/button-->
+      </a>
+      </b-->
+
+
+    </div>
+
+  </div>
+</html>
